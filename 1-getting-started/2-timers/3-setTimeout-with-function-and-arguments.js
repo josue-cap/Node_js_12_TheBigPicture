2ea5ba35d1c4c71301de51a20ba73dd7361bfb6d
josue-cap <josue.alvarado.cap@gmail.com>
2023-07-13 16:49:32 -0400

    challenge 1 finished

--- 1-getting-started/2-timers/3-setTimeout-with-function-and-arguments.js
+++ 1-getting-started/2-timers/3-setTimeout-with-function-and-arguments.js
@@ -1,5 +1,5 @@
 const rocks = who => {
   console.log(who + ' rocks');
 };
-
+//TODO: en este Timeout el tercer argumento que es 'Pluralsight' pasa a la funcion rocks como parametro who
 setTimeout(rocks, 2 * 1000, 'Pluralsight');
--- 1-getting-started/2-timers/4-challenge1.js
+++ 1-getting-started/2-timers/4-challenge1.js
@@ -1,9 +1,11 @@
-const theOneFunc = () => {};
-
-setTimeout(theOneFunc, 4 * 1000);
-
+const theOneFunc = param => {
+    console.log('Hello in: ' + param + ' seconds.!');
+};
+                           //TODO: el tercer argumento es params que pasa a theOneFunc
 // Hello after 4 seconds
-
+setTimeout(theOneFunc, 4 * 1000, 4);
 // Hello after 8 seconds
+setTimeout(theOneFunc, 8 * 1000, 8);
 
 // You can define only ONE function
+    
